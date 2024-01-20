bd6e2012e57c96e7a09eb85dd9ab36580c8e535d
leejongseok1 <twocvx1@gmail.com>
2024-01-20 16:38:38 +0900

    240120

--- 디자인에_따른_유저_체류시간_AB테스트.py
+++ 디자인에_따른_유저_체류시간_AB테스트.py
@@ -1,1 +1,10 @@
-import scipy
+from scipy import stats
+
+# 데이터
+design_A = [16.4, 12.6, 17.5, 18.8, 12.1, 13.2, 14.5, 12.4, 17.5, 25.4, 9.3, 10.4]
+design_B = [12.1, 11.8, 14.7, 13.1, 13.8, 10.1, 9.1, 13.5, 11.2, 13.7]
+
+# 검정 코드
+stats.ttest_ind(design_A,
+                design_B,
+                equal_var=False)
